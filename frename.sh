bbda8a3faf3ddc4f724f37bc573cbf0d3a334bf4
Nikolas Hunter <166237108+NikolasHunter515@users.noreply.github.com>
2025-12-20 22:33:29 -0500

    Bump version to 1.1.0 and add safety parameter
    Updated version to 1.1.0 and added rename safety option.

--- frename.sh
+++ frename.sh
@@ -5,12 +5,17 @@ source ./renameUtils.sh;
 #get season num from path
 #change directory
 #change all files in directory.
-VERSION="1.0.0";
+VERSION="1.1.0";
 param=$1;
+param2="true"; #added a rename safety
 
 if [[ "$1" == "--version" || "$1" == "-v" ]]; then
         echo "Frename version $VERSION";
         exit 0;
+fi
+
+if [[ "$2" == "-t" ]]; then
+        param2="false";
 fi
 
 path="tvshows/sopranos/season 1";
