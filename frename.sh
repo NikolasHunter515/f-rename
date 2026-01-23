3c6ce70307957bba9b7bab7f7a91f61fa23d8944
Nikolas Hunter <166237108+NikolasHunter515@users.noreply.github.com>
2026-01-23 13:42:47 -0500

    Enhance frename.sh with safety flags and options
    Added flags for rename safety and zero flag handling.

--- frename.sh
+++ frename.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 source ./renameUtils.sh;
 
-#plan
-#get season num from path
-#change directory
-#change all files in directory.
+#TODO add safe guard for flags, need to handle unexpected usage.
 VERSION="1.1.1";
 param=$1;
-param2="true"; #added a rename safety
+param2="false"; #added a rename safety
+param3="false"; #zero flag
 
 if [[ "$1" == "--version" || "$1" == "-v" ]]; then
         echo "Frename version $VERSION";
@@ -15,12 +13,32 @@ if [[ "$1" == "--version" || "$1" == "-v" ]]; then
 fi
 
 if [[ "$2" == "-t" ]]; then
-        param2="false";
+        param2="true";
 fi
+
+if [[ "$3" == "-z" ]]; then
+        param3="true";
+fi
+
+cd "$param" || { echo "Invalid directory: $param"; exit 1; }
 
 p1=$(numExtract "$param");
 cd "$param";
-pwd
-echo "Script starting";
 
-renameAll $p1;
+#unsorted rename
+if [[ "$4" == "-uS" ]]; then
+        unsorted_rename $p1 "$param3" "$param2";
+        exit 0;
+fi
+
+#sorted rename or rename all.
+if [[ "$4" == "-sS" ]]; then
+        renameAll $p1 "$param2" "$param3" "true";
+        exit 0;
+fi
+
+path="tvshows/sopranos/season 1";
+pwd
+echo "working";
+
+renameAll $p1 "$param2" "$param3" "false";
