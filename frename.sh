5abbac5bb5af82b247ee4cdcf13a748a09b63d9f
Nikolas Hunter <166237108+NikolasHunter515@users.noreply.github.com>
2026-01-23 16:49:36 -0500

    Refactor argument parsing and rename logic

--- frename.sh
+++ frename.sh
@@ -3,42 +3,72 @@ source ./renameUtils.sh;
 
 #TODO add safe guard for flags, need to handle unexpected usage.
 VERSION="1.1.1";
-param=$1;
+param="";
 param2="false"; #added a rename safety
 param3="false"; #zero flag
+mode="";
 
-if [[ "$1" == "--version" || "$1" == "-v" ]]; then
-        echo "Frename version $VERSION";
-        exit 0;
-fi
+# parse args
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -v|--version)
+            echo "Frename version $VERSION"
+            exit 0
+            ;;
+        -t)
+            param2="true"
+            ;;
+        -z)
+            param3="true"
+            ;;
+        -uS)
+            mode="unsorted"
+            ;;
+        -sS)
+            mode="sorted"
+            ;;
+        -*)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+        *)
+            # first non-flag argument = directory
+            if [[ -z "$param" ]]; then
+                param="$1"
+            else
+                echo "Unexpected argument: $1"
+                exit 1
+            fi
+            ;;
+    esac
+    shift
+done
 
-if [[ "$2" == "-t" ]]; then
-        param2="true";
-fi
-
-if [[ "$3" == "-z" ]]; then
-        param3="true";
+#check before changing dir.
+if [[ -z "$param" ]]; then
+    echo "Usage: frename [options] <directory>"
+    exit 1
 fi
 
 cd "$param" || { echo "Invalid directory: $param"; exit 1; }
 
 p1=$(numExtract "$param");
-cd "$param";
 
-#unsorted rename
-if [[ "$4" == "-uS" ]]; then
-        unsorted_rename $p1 "$param3" "$param2";
-        exit 0;
-fi
-
-#sorted rename or rename all.
-if [[ "$4" == "-sS" ]]; then
-        renameAll $p1 "$param2" "$param3" "true";
-        exit 0;
-fi
+case "$mode" in
+    unsorted)
+        echo "Unsorted rename";
+        unsorted_rename "$p1" "$param3" "$param2"
+        exit 1
+        ;;
+    sorted)
+        echo "Sorted rename";
+        renameAll "$p1" "$param2" "$param3" "true"
+        exit 1
+        ;;
+esac
 
 path="tvshows/sopranos/season 1";
 pwd
-echo "working";
+echo "Numeric extract rename";
 
 renameAll $p1 "$param2" "$param3" "false";
